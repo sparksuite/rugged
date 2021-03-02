096d04d9d4bf870b979f5a32e76a31feb01dd979
Wes Cossick <wes@sparksuite.com>
2021-03-02 09:52:42 -0600

    Randomize the mutex file name

--- src/utils/configure.ts
+++ src/utils/configure.ts
@@ -1,3 +1,6 @@
+// Imports
+import tmp from 'tmp';
+
 // Define what configuration looks like
 export interface Configuration {
 	testProjectsDirectory: string;
@@ -9,6 +12,6 @@ export default async function configure(): Promise<Configuration> {
 	// Return
 	return {
 		testProjectsDirectory: 'test-projects',
-		yarnMutexFilePath: '/tmp/.yarn-mutex', // TODO: Make random
+		yarnMutexFilePath: tmp.fileSync().name,
 	};
 }
