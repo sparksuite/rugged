ebbdb91562928fba97954e5bf11e15c6753dc9e5
Wes Cossick <wes@sparksuite.com>
2021-03-03 15:18:31 -0600

    Improve verify tests

--- src/utils/verify.test.ts
+++ src/utils/verify.test.ts
@@ -4,14 +4,19 @@ import path from 'path';
 import { HandledError } from './errors';
 import configure from './configure';
 
-// Mock the function
+// Mock the log function
 console.log = jest.fn();
+
+// Set the absolute path to the test file trees directory
+const testFileTreesPath = path.normalize(path.join(__dirname, '..', '..', 'test-file-trees'));
 
 // Tests
 describe('#verify.packageFile()', () => {
 	it('Catches missing package files', () => {
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'missing-package'));
+
 		expect(() => {
-			verify.packageFile(path.normalize(path.join(__dirname, '..', '..', 'test-file-trees', 'missing-package')));
+			verify.packageFile();
 		}).toThrow(HandledError);
 
 		expect(console.log).toHaveBeenCalledTimes(1);
@@ -19,8 +24,10 @@ describe('#verify.packageFile()', () => {
 	});
 
 	it('Catches package files that are missing a name', () => {
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'package-missing-name'));
+
 		expect(() => {
-			verify.packageFile(path.normalize(path.join(__dirname, '..', '..', 'test-file-trees', 'package-missing-name')));
+			verify.packageFile();
 		}).toThrow(HandledError);
 
 		expect(console.log).toHaveBeenCalledTimes(1);
@@ -28,9 +35,9 @@ describe('#verify.packageFile()', () => {
 	});
 
 	it('Returns parsed version', () => {
-		expect(
-			verify.packageFile(path.normalize(path.join(__dirname, '..', '..', 'test-file-trees', 'primary')))
-		).toStrictEqual({
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'primary'));
+
+		expect(verify.packageFile()).toStrictEqual({
 			name: 'primary',
 		});
 	});
@@ -38,13 +45,12 @@ describe('#verify.packageFile()', () => {
 
 describe('#verify.testProjects()', () => {
 	it('Catches missing package files', async () => {
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'missing-test-projects'));
+
 		const configuration = await configure();
 
 		expect(() => {
-			verify.testProjects(
-				configuration,
-				path.normalize(path.join(__dirname, '..', '..', 'test-file-trees', 'missing-test-projects'))
-			);
+			verify.testProjects(configuration);
 		}).toThrow(HandledError);
 
 		expect(console.log).toHaveBeenCalledTimes(1);
@@ -54,10 +60,10 @@ describe('#verify.testProjects()', () => {
 	});
 
 	it('Returns absolute path', async () => {
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'primary'));
+
 		const configuration = await configure();
 
-		expect(
-			verify.testProjects(configuration, path.normalize(path.join(__dirname, '..', '..', 'test-file-trees', 'primary')))
-		).toMatch(/\/rugged\/test\-file\-trees\/primary\/test\-projects$/);
+		expect(verify.testProjects(configuration)).toMatch(/\/rugged\/test\-file\-trees\/primary\/test\-projects$/);
 	});
 });
