2812b16252e866825cf854fd1afe7ce980b3f1bc
Wes Cossick <wes@sparksuite.com>
2021-03-04 09:12:20 -0600

    Format the code

--- src/utils/get-config.test.ts
+++ src/utils/get-config.test.ts
@@ -10,7 +10,7 @@ const testFileTreesPath = path.normalize(path.join(__dirname, '..', '..', 'test-
 describe('#getConfig(true)', () => {
 	it('Handles no config file', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'no-config'));
-		
+
 		expect(await getConfig(true)).toMatchObject({
 			injectAsDevDependency: false,
 		});
@@ -18,7 +18,7 @@ describe('#getConfig(true)', () => {
 
 	it('Skips reconstructing the config when possible', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'no-config'));
-		
+
 		const config1 = await getConfig(true);
 		const config2 = await getConfig();
 
@@ -27,7 +27,7 @@ describe('#getConfig(true)', () => {
 
 	it('Handles JS custom config files', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'js-config'));
-		
+
 		expect(await getConfig(true)).toMatchObject({
 			injectAsDevDependency: true,
 		});
@@ -35,22 +35,24 @@ describe('#getConfig(true)', () => {
 
 	it('Handles invalid config file export', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'invalid-config'));
-		
+
 		await expect(() => getConfig(true)).rejects.toThrow(PrintableError);
 		await expect(() => getConfig(true)).rejects.toThrow('The rugged.config.js file doesn’t export an object');
 	});
 
 	it('Handles unknown config keys', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'unknown-config-key'));
-		
+
 		await expect(() => getConfig(true)).rejects.toThrow(PrintableError);
 		await expect(() => getConfig(true)).rejects.toThrow('The rugged.config.js file contains an unrecognized key: fake');
 	});
 
 	it('Handles invalid config key values', async () => {
 		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'invalid-config-value'));
-		
+
 		await expect(() => getConfig(true)).rejects.toThrow(PrintableError);
-		await expect(() => getConfig(true)).rejects.toThrow('In the rugged.config.js file, the injectAsDevDependency key contains an invalid value');
+		await expect(() => getConfig(true)).rejects.toThrow(
+			'In the rugged.config.js file, the injectAsDevDependency key contains an invalid value'
+		);
 	});
 });
