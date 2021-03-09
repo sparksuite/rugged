14e96057edf6a505cafe3fcd3c1344c28c43325a
Cory Harper <zepher2211@gmail.com>
2021-03-09 14:37:45 -0600

    added printing of successful error messages based on config

--- src/steps/test-projects.ts
+++ src/steps/test-projects.ts
@@ -10,7 +10,7 @@ import printHeader from '../utils/print-header';
 /** Installs dependencies into the root project and test projects */
 export default async function testProjects(testProjectPaths: string[], finalResult: FinalResult) {
 	// Get the configuration for testing in parallel
-	const { testInParallel } = await getConfig();
+	const { testInParallel, printSuccessfulOutput } = await getConfig();
 
 	// Print section header
 	printHeader('Testing projects');
@@ -23,16 +23,30 @@ export default async function testProjects(testProjectPaths: string[], finalResu
 				execa('yarn', [`test`], {
 					cwd: testProjectPath,
 					all: true,
-				}).catch((error) => {
-					// Add to final result
-					finalResult.failedTests.push({
-						project: path.basename(testProjectPath),
-						output: error.all ?? 'No output...',
-					});
+				}).then(
+					printSuccessfulOutput
+						? (result) => {
+								// Add to final result
+								finalResult.successfulTests.push({
+									project: path.basename(testProjectPath),
+									output: result.all ?? 'No output...',
+								});
 
-					// Throw error that Listr will pick up
-					throw new Error('Output will be printed below');
-				}),
+								// Throw error that Listr will pick up
+								throw new Error('Output will be printed below');
+						  }
+						: undefined,
+					(error) => {
+						// Add to final result
+						finalResult.failedTests.push({
+							project: path.basename(testProjectPath),
+							output: error.all ?? 'No output...',
+						});
+
+						// Throw error that Listr will pick up
+						throw new Error('Output will be printed below');
+					}
+				),
 		})),
 		{
 			concurrent: testInParallel,
