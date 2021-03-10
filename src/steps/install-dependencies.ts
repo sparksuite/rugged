a81fc67e958644207ec403f132b40cab5a326e48
Cory Harper <zepher2211@gmail.com>
2021-03-10 15:09:13 -0600

    updated method of getting package file

--- src/steps/install-dependencies.ts
+++ src/steps/install-dependencies.ts
@@ -4,13 +4,16 @@ import Listr from 'listr';
 import path from 'path';
 import { HandledError, yarnErrorCatcher } from '../utils/errors';
 import getConfig from '../utils/get-config';
+import getPackageFile from '../utils/get-package-file';
 import printHeader from '../utils/print-header';
-import { PackageFile } from '../utils/verify';
 
 /** Installs dependencies into the root project and test projects */
-export default async function installDependencies(packageFile: PackageFile, testProjectPaths: string[]) {
+export default async function installDependencies(testProjectPaths: string[]) {
 	// Print section header
 	printHeader('Installing dependencies');
+
+	// Get the package file
+	const packageFile = getPackageFile();
 
 	// Get config
 	const config = await getConfig();
