76e33a675b8aebe03641e2426ed84409afca72e3
Cory Harper <zepher2211@gmail.com>
2021-03-11 14:32:19 -0600

    Modified test to make output on failure more useful

--- src/utils/get-config.test.ts
+++ src/utils/get-config.test.ts
@@ -22,7 +22,7 @@ describe('#getConfig(true)', () => {
 		const config1 = await getConfig(true);
 		const config2 = await getConfig();
 
-		expect(config1).toBe(config2);
+		expect(Object.is(config2, config1)).toBe(true);
 	});
 
 	it('Handles JS custom config files', async () => {
