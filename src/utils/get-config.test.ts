8c5c24df1f6451701ad2660a90002fe2c7a475b5
Cory Harper <zepher2211@gmail.com>
2021-03-11 13:10:44 -0600

    refactored test to check for object identity

--- src/utils/get-config.test.ts
+++ src/utils/get-config.test.ts
@@ -22,7 +22,7 @@ describe('#getConfig(true)', () => {
 		const config1 = await getConfig(true);
 		const config2 = await getConfig();
 
-		expect(config1.yarnMutexFilePath).toStrictEqual(config2.yarnMutexFilePath);
+		expect(config1).toBe(config2);
 	});
 
 	it('Handles JS custom config files', async () => {
