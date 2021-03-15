8fcf287a186bc48d65d26e87e416ab35164d9118
Cory Harper <zepher2211@gmail.com>
2021-03-15 18:03:03 -0500

    fixed linting errors in print header

--- src/utils/print-header.ts
+++ src/utils/print-header.ts
@@ -2,6 +2,6 @@
 import chalk from './chalk';
 
 /** Print a stylized section header */
-export default function printHeader(header: string) {
+export default function printHeader(header: string): void {
 	console.log(`\n${chalk.inverse(chalk.bold(` ${header} `))}\n`);
 }
