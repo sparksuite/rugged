f325625803481c42d4cd0fa97f8cfacf1d63b1bd
Cory Harper <zepher2211@gmail.com>
2021-03-18 11:02:56 -0500

    brought install dependencies tests in line with changes to mocking

--- src/steps/install-dependencies.test.ts
+++ src/steps/install-dependencies.test.ts
@@ -1,85 +1,109 @@
 // Imports
-import Listr from "listr";
-import path from "path";
-import { HandledError } from "../utils/errors";
-import getContext from "../utils/get-context";
-import packageManager from "../utils/package-manager";
-import printHeader from "../utils/print-header";
-import installDependencies from "./install-dependencies";
+import path from 'path';
+
+// Initialize
+let getContext: jest.Mock;
+let printHeader: jest.Mock;
+let execa: jest.Mock;
 
 // Mocks
 jest.mock('../utils/print-header');
 jest.mock('../utils/get-context');
-jest.mock('listr');
-
-(Listr.prototype.run as jest.Mock).mockImplementation(() => Promise.resolve());
-
-(getContext as jest.Mock).mockImplementation(() => ({
-    packageFile: {
-        name: 'example',
-    },
-}));
-
-jest.mock('execa', () => ({
-    __esModule: true,
-    default: () => Promise.resolve(),
-}));
-
-jest.spyOn(packageManager, 'pack');
-jest.spyOn(packageManager, 'remove');
-jest.spyOn(packageManager, 'unlink');
-jest.spyOn(packageManager, 'runScript');
-jest.spyOn(packageManager, 'errorCatcher');
-jest.spyOn(packageManager, 'choosePackageManager').mockReturnValue(Promise.resolve('yarn'));
-jest.spyOn(path, 'basename').mockReturnValue('example');
+jest.mock('../utils/package-manager');
+jest.mock('execa');
 
 // Tests
 describe('#installDependencies()', () => {
-    it('Prints header', async () => {
-        await installDependencies(['/example-project']);
+	beforeEach(() => {
+		jest.resetModules();
+		printHeader = require('../utils/print-header').default;
+		getContext = require('../utils/get-context').default;
+		execa = require('execa');
+		getContext.mockImplementation(() => ({
+			packageFile: {
+				name: 'example',
+			},
+		}));
 
-        expect(printHeader).toHaveBeenCalledTimes(1);
-        expect(printHeader).toHaveBeenCalledWith("Installing dependencies");
-    });
+		const packageManager = require('../utils/package-manager').default;
 
-    it('Retrieves the context', async () => {
-        await installDependencies(['/example-project']);
+		packageManager.installDependencies.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.choosePackageManager.mockReturnValue(Promise.resolve('yarn'));
+		packageManager.errorCatcher.mockImplementation(() => {
+			throw new Error();
+		});
+		jest.spyOn(path, 'basename').mockReturnValue('example');
+	});
 
-        expect(getContext).toHaveBeenCalledTimes(1);
-    });
+	it('Prints header', async () => {
+		execa.mockImplementation(() => Promise.resolve());
+		const installDependencies = require('./install-dependencies').default;
 
-    it('Creates a list of tasks', async () => {
-        await installDependencies(['/example-project']);
+		await installDependencies(['/example-project']);
 
-        expect(Listr).toHaveBeenCalledTimes(1);
-        expect(Listr).toHaveBeenCalledWith(
-            [
-                {
-                    title: 'example',
-                    task: expect.any(Function),
-                },
-                {
-                    title: 'Project: example',
-                    task: expect.any(Function),
-                },
-            ],
-            {
-                concurrent: true,
-                exitOnError: false,
-            }
-        );
-    });
+		expect(printHeader).toHaveBeenCalledTimes(1);
+		expect(printHeader).toHaveBeenCalledWith('Installing dependencies');
+	});
 
-    it("Runs it's tasks", async () => {
-        jest.mock('listr');
-        await installDependencies(['/example-project']);
+	it('Retrieves the context', async () => {
+		execa.mockImplementation(() => Promise.resolve());
+		const installDependencies = require('./install-dependencies').default;
 
-        expect((Listr as jest.Mock<Listr>).mock.instances[0].run).toHaveBeenCalledTimes(1);
-    });
+		await installDependencies(['/example-project']);
 
-    it("Gracefully handles promise rejections", async () => {
-        (Listr.prototype.run as jest.Mock).mockImplementationOnce(() => Promise.reject());
+		expect(getContext).toHaveBeenCalledTimes(1);
+	});
 
-        await expect(() => installDependencies(['/example-project'])).rejects.toThrow(HandledError);
-    });
-})
+	it('Creates a list of tasks', async () => {
+		jest.doMock('listr');
+		const Listr = require('listr');
+
+		execa.mockImplementation(() => Promise.resolve());
+
+		(Listr.prototype.run as jest.Mock).mockImplementation(() => Promise.resolve());
+
+		const installDependencies = require('./install-dependencies').default;
+
+		await installDependencies(['/example-project']);
+
+		expect(Listr).toHaveBeenCalledTimes(1);
+		expect(Listr).toHaveBeenCalledWith(
+			[
+				{
+					title: 'example',
+					task: expect.any(Function),
+				},
+				{
+					title: 'Project: example',
+					task: expect.any(Function),
+				},
+			],
+			{
+				concurrent: true,
+				exitOnError: false,
+			}
+		);
+
+		jest.dontMock('listr');
+	});
+
+	it("Runs it's tasks", async () => {
+		execa.mockImplementation(() => Promise.resolve());
+
+		const installDependencies = require('./install-dependencies').default;
+
+		await expect(installDependencies(['/example-project'])).resolves.not.toThrow();
+		expect(execa).toHaveBeenCalledTimes(2);
+		expect(execa).toHaveBeenCalledWith('yarn', []);
+		expect(execa).toHaveBeenCalledWith('yarn', [], { cwd: '/example-project' });
+	});
+
+	it('Gracefully handles promise rejections', async () => {
+		execa.mockImplementation(() => Promise.reject());
+
+		const { HandledError } = require('../utils/errors');
+		const installDependencies = require('./install-dependencies').default;
+
+		await expect(installDependencies(['/example-project'])).rejects.toThrow(HandledError);
+	});
+});
