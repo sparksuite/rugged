acf48aea82f4378c22d3d260e424c6dc3d67ace2
Cory Harper <zepher2211@gmail.com>
2021-03-18 11:00:18 -0500

    overhauled approach to mocking, updated tests

--- src/steps/inject-root-package.test.ts
+++ src/steps/inject-root-package.test.ts
@@ -1,82 +1,146 @@
 // Imports
-import Listr from "listr";
-import getConfig from "../utils/get-config";
-import getContext from "../utils/get-context";
-import packageManager from "../utils/package-manager";
-import printHeader from "../utils/print-header";
-import injectRootPackage from "./inject-root-package";
+import path from 'path';
+
+// Initialize
+let getContext: jest.Mock;
+let getConfig: jest.Mock;
+let printHeader: jest.Mock;
+let execa: jest.Mock;
 
 // Mocks
 jest.mock('../utils/print-header');
-jest.mock('../utils/get-context', () => ({
-    __esModule: true,
-    default: () => ({
-        packageFile: {
-            name: 'example',
-        },
-    })
-}));
-jest.mock('../utils/get-config', () => ({
-    __esModule: true,
-    default: () => ({
-        injectAsDevDependency: false,
-    })
-}));
-
+jest.mock('../utils/get-context');
+jest.mock('../utils/get-config');
+jest.mock('../utils/package-manager');
+jest.mock('tmp');
 jest.mock('execa');
-jest.mock('listr');
-
-Listr.prototype.run = jest.fn().mockImplementation(() => Promise.resolve());
-
-jest.spyOn(packageManager, 'pack');
-jest.spyOn(packageManager, 'remove');
-jest.spyOn(packageManager, 'unlink');
-jest.spyOn(packageManager, 'runScript');
-jest.spyOn(packageManager, 'errorCatcher');
-jest.spyOn(packageManager, 'choosePackageManager').mockReturnValue(Promise.resolve('yarn'));
 
 // Tests
 describe('#injectRootPackage()', () => {
-    it('Retrieves the context', async () => {
-        await injectRootPackage(['/example-project']);
+	beforeEach(() => {
+		// Resetting modules due to doMock
+		jest.resetModules();
 
-        expect(getContext).toHaveBeenCalledTimes(1);
-    });
+		// Modules have to be re-required after calls to doMock, or you get a different instance of the module than is used in testing
+		printHeader = require('../utils/print-header').default;
+		getContext = require('../utils/get-context').default;
+		getConfig = require('../utils/get-config').default;
+		execa = require('execa');
+		getContext.mockImplementation(() => ({
+			packageFile: {
+				name: 'example',
+				scripts: {
+					compile: '',
+				},
+			},
+		}));
+		getConfig.mockImplementation(() => ({
+			injectAsDevDependency: false,
+			testProjectsDirectory: 'test-projects',
+			yarnMutexPort: 31997,
+			testInParallel: true,
+			compileScriptName: 'compile',
+			printSuccessfulOutput: false,
+		}));
+		const tmp = require('tmp');
 
-    it('Prints header', async () => {
-        await injectRootPackage(['/example-project']);
+		tmp.dirSync.mockReturnValue({ name: '/example-directory' });
 
-        expect(printHeader).toHaveBeenCalledTimes(1);
-        expect(printHeader).toHaveBeenCalledWith("Injecting example");
-    });
+		const packageManager = require('../utils/package-manager').default;
 
-    it('Retrieves the config', async () => {
-        await injectRootPackage(['/example-project']);
+		packageManager.runScript.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.pack.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.remove.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.unlink.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.add.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.choosePackageManager.mockReturnValue(Promise.resolve('yarn'));
+		packageManager.errorCatcher.mockImplementation(() => {
+			throw new Error();
+		});
+		jest.spyOn(path, 'basename').mockReturnValue('example');
+	});
 
-        expect(getConfig).toHaveBeenCalledTimes(1);
-    });
+	it('Prints header', async () => {
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+		const injectRootPackage = require('./inject-root-package').default;
 
-    it('Creates a list of tasks', async () => {
-        await injectRootPackage(['/example-project']);
+		await injectRootPackage(['/example-project']);
 
-        expect(Listr).toHaveBeenCalledWith([
-            {
-                title: 'Compiling',
-                skip: expect.any(Function),
-                task: expect.any(Function),
-            },
-            {
-                title: 'Packaging',
-                task: expect.any(Function),
-            },
-            {
-                title: 'Removing linked version',
-                task: expect.any(Function),
-            },
-            {
-                title: 'Injecting packaged version',
-                task: expect.any(Function),
-            },
-        ]);
-    });
-})
+		expect(printHeader).toHaveBeenCalledTimes(1);
+		expect(printHeader).toHaveBeenCalledWith('Injecting example');
+	});
+
+	it('Retrieves the context', async () => {
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+		const injectRootPackage = require('./inject-root-package').default;
+
+		await injectRootPackage(['/example-project']);
+
+		expect(getContext).toHaveBeenCalledTimes(1);
+	});
+
+	it('Creates a list of tasks', async () => {
+		jest.doMock('listr');
+		const Listr = require('listr');
+
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+
+		(Listr.prototype.run as jest.Mock).mockImplementation(() => Promise.resolve());
+
+		const injectRootPackage = require('./inject-root-package').default;
+
+		await injectRootPackage(['/example-project']);
+
+		expect(Listr).toHaveBeenCalledTimes(1);
+		expect(Listr).toHaveBeenCalledWith([
+			{
+				title: 'Compiling',
+				skip: expect.any(Function),
+				task: expect.any(Function),
+			},
+			{
+				title: 'Packaging',
+				task: expect.any(Function),
+			},
+			{
+				title: 'Removing linked version',
+				task: expect.any(Function),
+			},
+			{
+				title: 'Injecting packaged version',
+				task: expect.any(Function),
+			},
+		]);
+
+		jest.dontMock('listr');
+	});
+
+	it("Runs it's tasks", async () => {
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+
+		const injectRootPackage = require('./inject-root-package').default;
+
+		await expect(injectRootPackage(['/example-project'])).resolves.not.toThrow();
+		expect(execa).toHaveBeenCalledTimes(5);
+		expect(execa).toHaveBeenCalledWith('yarn', []);
+		expect(execa).toHaveBeenCalledWith('yarn', [], { cwd: '/example-project' });
+	});
+
+	it('Gracefully handles promise rejections', async () => {
+		execa.mockImplementation(() => Promise.reject());
+
+		const { HandledError } = require('../utils/errors');
+		const injectRootPackage = require('./inject-root-package').default;
+
+		await expect(injectRootPackage(['/example-project'])).rejects.toThrow(HandledError);
+	});
+
+	it('Throws an error on the Packaging step if there is no output from stdout', async () => {
+		execa.mockImplementation(() => Promise.resolve());
+
+		const { HandledError } = require('../utils/errors');
+		const injectRootPackage = require('./inject-root-package').default;
+
+		await expect(injectRootPackage(['/example-project'])).rejects.toThrow(HandledError);
+	});
+});
