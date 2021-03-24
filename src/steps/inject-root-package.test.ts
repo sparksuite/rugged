fbe267e6e15bd1f25033d88ec2132988cafee7fe
Cory Harper <zepher2211@gmail.com>
2021-03-24 17:43:43 -0500

    increased coverage for injectRootPackage on conditionals

--- src/steps/inject-root-package.test.ts
+++ src/steps/inject-root-package.test.ts
@@ -1,10 +1,10 @@
 // Imports
 import ListrDef from 'listr';
-import path from 'path';
 import unmockedPackageManager from '../utils/package-manager';
 import injectRootPackageDef from './inject-root-package';
 import tmpDef from 'tmp';
 import * as Errors from '../utils/errors';
+import fsDef from 'fs';
 
 // Initialize
 let getContext: jest.Mock;
@@ -19,6 +19,7 @@ jest.mock('../utils/get-config');
 jest.mock('../utils/package-manager');
 jest.mock('tmp');
 jest.mock('execa');
+jest.mock('fs');
 
 // Tests
 describe('#injectRootPackage()', () => {
@@ -64,7 +65,6 @@ describe('#injectRootPackage()', () => {
 		packageManager.errorCatcher.mockImplementation(() => {
 			throw new Error();
 		});
-		jest.spyOn(path, 'basename').mockReturnValue('example');
 	});
 
 	it('Prints header', async () => {
@@ -154,5 +154,111 @@ describe('#injectRootPackage()', () => {
 		const injectRootPackage = (require('./inject-root-package') as { default: typeof injectRootPackageDef }).default;
 
 		await expect(injectRootPackage(['/example-project'])).rejects.toThrow(HandledError);
+	});
+
+	it('Manually moves the file to the temporary directory, if using npm', async () => {
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+		const packageManager = (require('../utils/package-manager') as {
+			default: jest.Mocked<typeof unmockedPackageManager>;
+		}).default;
+		const fs = require('fs') as jest.Mocked<typeof fsDef>;
+
+		packageManager.choosePackageManager.mockReturnValue(Promise.resolve('npm'));
+
+		const injectRootPackage = (require('./inject-root-package') as { default: typeof injectRootPackageDef }).default;
+
+		await injectRootPackage(['/example-project']);
+
+		expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
+		expect(fs.copyFileSync).toHaveBeenCalledWith(expect.any(String), expect.any(String));
+		expect(fs.unlinkSync).toHaveBeenCalledTimes(1);
+		expect(fs.unlinkSync).toHaveBeenCalledWith(expect.any(String));
+	});
+
+	it('Gracefully handles promise rejections during unlink phase', async () => {
+		const packageManager = (require('../utils/package-manager') as {
+			default: jest.Mocked<typeof unmockedPackageManager>;
+		}).default;
+
+		packageManager.remove.mockReturnValue(Promise.resolve({ args: ['throw'], tool: 'yarn' }));
+		packageManager.unlink.mockReturnValue(Promise.resolve({ args: ['throw'], tool: 'yarn' }));
+		execa.mockImplementation((_, args) => {
+			if (!Array.isArray(args)) {
+				return;
+			}
+
+			if (args.length === 0) {
+				return Promise.resolve({ stdout: '' });
+			}
+
+			return Promise.reject(new Error());
+		});
+
+		const { HandledError } = require('../utils/errors') as typeof Errors;
+		const injectRootPackage = (require('./inject-root-package') as { default: typeof injectRootPackageDef }).default;
+
+		await expect(injectRootPackage(['/example-project'])).rejects.toThrow(HandledError);
+		expect(packageManager.errorCatcher).toHaveBeenCalledTimes(1);
+
+		packageManager.errorCatcher.mockClear();
+		packageManager.remove.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+
+		await expect(injectRootPackage(['/example-project'])).rejects.toThrow(HandledError);
+		expect(packageManager.errorCatcher).toHaveBeenCalledTimes(1);
+	});
+
+	it('Ignores expected errors in the unlink stage', async () => {
+		const packageManager = (require('../utils/package-manager') as {
+			default: jest.Mocked<typeof unmockedPackageManager>;
+		}).default;
+		const injectRootPackage = (require('./inject-root-package') as { default: typeof injectRootPackageDef }).default;
+
+		execa.mockImplementation((_, args) => {
+			if (!Array.isArray(args)) {
+				return;
+			}
+
+			if (args.length === 0) {
+				return Promise.resolve({ stdout: '' });
+			}
+
+			return Promise.reject(new Error("This module isn't specified in a package.json file"));
+		});
+		packageManager.remove.mockReturnValue(Promise.resolve({ args: ['throw'], tool: 'yarn' }));
+		packageManager.unlink.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+
+		await expect(injectRootPackage(['/example-project'])).resolves.not.toThrow();
+		expect(packageManager.errorCatcher).toHaveBeenCalledTimes(0);
+
+		execa.mockImplementation((_, args) => {
+			if (!Array.isArray(args)) {
+				return;
+			}
+
+			if (args.length === 0) {
+				return Promise.resolve({ stdout: '' });
+			}
+
+			return Promise.reject(new Error('No registered package found called'));
+		});
+		packageManager.unlink.mockReturnValue(Promise.resolve({ args: ['throw'], tool: 'yarn' }));
+		packageManager.remove.mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+		packageManager.errorCatcher.mockClear();
+
+		await expect(injectRootPackage(['/example-project'])).resolves.not.toThrow();
+		expect(packageManager.errorCatcher).toHaveBeenCalledTimes(0);
+	});
+
+	it('Handles no scripts in package file', async () => {
+		execa.mockImplementation(() => Promise.resolve({ stdout: '' }));
+		getContext.mockImplementation(() => ({
+			packageFile: {
+				name: 'example',
+			},
+		}));
+
+		const injectRootPackage = (require('./inject-root-package') as { default: typeof injectRootPackageDef }).default;
+
+		await expect(injectRootPackage(['/example-project'])).resolves.not.toThrow();
 	});
 });
