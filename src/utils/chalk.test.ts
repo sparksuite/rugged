f700fb84a1a028437dd6d48e1eb693271d2cb397
Cory Harper <zepher2211@gmail.com>
2021-03-24 19:09:04 -0500

    tested branch path for chalk

---
+++ src/utils/chalk.test.ts
@@ -0,0 +1,17 @@
+// Imports
+import Chalk from 'chalk';
+
+
+// Tests
+describe('chalk', () => {
+    it('Sets its own color level outside of a test environment', () => {
+        process.env.NODE_ENV = 'development';
+
+        const chalk = (require('./chalk') as { default: Chalk.Chalk }).default;
+        expect(chalk.level).toEqual(expect.any(Number));
+    });
+
+    afterAll(() => {
+        process.env.NODE_ENV = 'test';
+    })
+})
