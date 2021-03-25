4eda1f9cc4f5536a86543de4927281ce8387695c
Cory Harper <zepher2211@gmail.com>
2021-03-24 19:09:50 -0500

    modified chalk to use env rather than jest worker variable

--- src/utils/chalk.ts
+++ src/utils/chalk.ts
@@ -1,7 +1,7 @@
 import Chalk from 'chalk';
 
 const chalk = new Chalk.Instance({
-	level: process.env.JEST_WORKER_ID ? 0 : undefined,
+	level: process.env.NODE_ENV === 'test' ? 0 : undefined,
 });
 
 export default chalk;
