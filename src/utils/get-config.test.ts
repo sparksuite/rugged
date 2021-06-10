8c33bf17eb8957c16247523ad4d5f44556630382
Cory Harper <zepher2211@gmail.com>
2021-06-10 15:30:47 -0500

    added test for positive timeout

--- src/utils/get-config.test.ts
+++ src/utils/get-config.test.ts
@@ -227,5 +227,12 @@ describe('#getConfig()', () => {
 		await expect(() => getConfig(true)).rejects.toThrow(
 			'In the rugged.config.js file, the timeouts key contains an invalid value'
 		);
+
+		jest.spyOn(process, 'cwd').mockReturnValue(path.join(testFileTreesPath, 'invalid-config-value', 'timeouts-value-not-positive-integer'));
+
+		await expect(() => getConfig(true)).rejects.toThrow(PrintableError);
+		await expect(() => getConfig(true)).rejects.toThrow(
+			'In the rugged.config.js file, the timeouts key contains an invalid value'
+		);
 	});
 });
