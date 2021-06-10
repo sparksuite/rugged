92d60c73e8c77f77a6ca14d0072e31fdbd858b42
Cory Harper <zepher2211@gmail.com>
2021-06-10 14:16:15 -0500

    fixed issue with tests on windows and tidied up test descriptions

--- src/utils/lockfile-manager.test.ts
+++ src/utils/lockfile-manager.test.ts
@@ -1,5 +1,6 @@
 // Imports
 import lockfileManager from './lockfile-manager';
+import path from 'path';
 import tmp from 'tmp';
 import fs from 'fs';
 
@@ -17,63 +18,81 @@ describe('#lockfileManager', () => {
 			lockfileManager.storeLockfile('/test');
 
 			expect(fs.mkdirSync).toHaveBeenCalledTimes(1);
-			expect(fs.mkdirSync).toHaveBeenCalledWith('/temp-dir/test');
+			expect(fs.mkdirSync).toHaveBeenCalledWith(path.join('/', 'temp-dir', 'test'));
 		});
 
-		it('Saves a temporary copy of package-lock when present', () => {
+		it('Saves a temporary copy of package-lock', () => {
 			(fs.existsSync as jest.Mock).mockImplementation(() => false);
 
 			lockfileManager.storeLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/test/package-lock.json', '/temp-dir/test/package-lock.json');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'test', 'package-lock.json'),
+				path.join('/', 'temp-dir', 'test', 'package-lock.json')
+			);
 		});
 
-		it('Saves a temporary copy of yarn.lock when present', () => {
+		it('Saves a temporary copy of yarn.lock', () => {
 			(fs.existsSync as jest.Mock).mockImplementation((path: string) => path.includes('yarn.lock'));
 
 			lockfileManager.storeLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/test/yarn.lock', '/temp-dir/test/yarn.lock');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'test', 'yarn.lock'),
+				path.join('/', 'temp-dir', 'test', 'yarn.lock')
+			);
 		});
 
-		it('Saves a temporary copy of npm-shrinkwrap when present', () => {
-			(fs.existsSync as jest.Mock).mockImplementation((path: string) => path.includes('npm-shrinkwrap'));
+		it('Saves a temporary copy of npm-shrinkwrap', () => {
+			(fs.existsSync as jest.Mock).mockImplementation((pathName: string) => pathName.includes('npm-shrinkwrap'));
 
 			lockfileManager.storeLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/test/npm-shrinkwrap.json', '/temp-dir/test/npm-shrinkwrap.json');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'test', 'npm-shrinkwrap.json'),
+				path.join('/', 'temp-dir', 'test', 'npm-shrinkwrap.json')
+			);
 		});
 	});
 
 	describe('#lockfileManager.overwriteLockfile()', () => {
-		it('Saves a temporary copy of package-lock when present', () => {
+		it('Overwrites package-json with the temporary copy', () => {
 			(fs.existsSync as jest.Mock).mockImplementation(() => false);
 
 			lockfileManager.overwriteLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/temp-dir/test/package-lock.json', '/test/package-lock.json');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'temp-dir', 'test', 'package-lock.json'),
+				path.join('/', 'test', 'package-lock.json')
+			);
 		});
 
-		it('Saves a temporary copy of yarn.lock when present', () => {
+		it('Overwrites yarn.lock with the temporary copy', () => {
 			(fs.existsSync as jest.Mock).mockImplementation((path: string) => path.includes('yarn.lock'));
 
 			lockfileManager.overwriteLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/temp-dir/test/yarn.lock', '/test/yarn.lock');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'temp-dir', 'test', 'yarn.lock'),
+				path.join('/', 'test', 'yarn.lock')
+			);
 		});
 
-		it('Saves a temporary copy of npm-shrinkwrap when present', () => {
+		it('Overwrites npm-shrinkwrap with the temporary copy', () => {
 			(fs.existsSync as jest.Mock).mockImplementation((path: string) => path.includes('npm-shrinkwrap'));
 
 			lockfileManager.overwriteLockfile('/test');
 
 			expect(fs.copyFileSync).toHaveBeenCalledTimes(1);
-			expect(fs.copyFileSync).toHaveBeenCalledWith('/temp-dir/test/npm-shrinkwrap.json', '/test/npm-shrinkwrap.json');
+			expect(fs.copyFileSync).toHaveBeenCalledWith(
+				path.join('/', 'temp-dir', 'test', 'npm-shrinkwrap.json'),
+				path.join('/', 'test', 'npm-shrinkwrap.json')
+			);
 		});
 	});
 });
