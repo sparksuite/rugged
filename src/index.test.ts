227e5a411cdc3ce907e68382b198c11fde4b636f
Cory Harper <zepher2211@gmail.com>
2021-07-08 11:33:00 -0500

    changed approach to testing entry point to a more stable approach

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,15 +1,17 @@
 // Imports
 import glob from 'glob';
-import { FinalResult } from '.';
+import injectRootPackage from './steps/inject-root-package';
+import installDependencies from './steps/install-dependencies';
 import testProjects from './steps/test-projects';
+import lockfile from './utils/lockfile';
 import packageManager from './utils/package-manager';
 
 jest.mock('./utils/print-header');
 jest.mock('./utils/get-context', () => ({
 	__esModule: true,
-	default: (): { packageFile: { name: 'example' } } => ({ packageFile: { name: 'example' } }),
+	default: (): { packageFile: { name: 'example' }, yarnAvailableGlobally: true, } => ({ packageFile: { name: 'example' }, yarnAvailableGlobally: true, }),
 }));
-jest.mock('./utils/package-manager');
+jest.mock('./utils/lockfile');
 jest.mock('./utils/verify');
 jest.mock('execa', () => ({
 	__esModule: true,
@@ -19,13 +21,15 @@ jest.mock('./steps/install-dependencies');
 jest.mock('./steps/inject-root-package');
 jest.mock('./steps/test-projects');
 jest.spyOn(glob, 'sync').mockReturnValue(['/example-project']);
-jest.spyOn(packageManager, 'remove').mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
-jest.spyOn(packageManager, 'add').mockReturnValue(Promise.resolve({ args: [], tool: 'yarn' }));
+jest.spyOn(packageManager, 'choosePackageManager').mockImplementation(() => Promise.resolve('yarn'));
+jest.spyOn(packageManager, 'remove').mockImplementation(() => Promise.resolve({ args: [], tool: 'yarn' }));
+jest.spyOn(packageManager, 'add').mockImplementation(() => Promise.resolve({ args: [], tool: 'yarn' }));
+jest.spyOn(packageManager, 'installDependencies').mockImplementation(() => Promise.resolve({ args: [], tool: 'yarn' }));
 jest.spyOn(packageManager, 'errorCatcher').mockImplementation(() => {
 	throw new Error();
 });
-jest.spyOn(console, 'log');
-(testProjects as jest.Mock).mockImplementation(async (_, finalResult: FinalResult) => {
+jest.spyOn(lockfile, 'overwrite').mockImplementation();
+(testProjects as jest.Mock).mockImplementation(async (_, finalResult: { successfulTests: { project: string, output: string, }[], failedTests: { project: string, output: string, }[] }) => {
 	finalResult.failedTests.push({
 		project: 'example-project',
 		output: 'Example error',
@@ -36,15 +40,17 @@ jest.spyOn(console, 'log');
 		output: 'Example output',
 	});
 });
-jest.spyOn(process, 'exit').mockImplementation(() => null as never);
+(installDependencies as jest.Mock).mockImplementation(async () => null);
+(injectRootPackage as jest.Mock).mockImplementation(async () => null);
+jest.spyOn(process, 'cwd').mockImplementation(() => '/');
+(process.exit as unknown as jest.Mock) = jest.fn();
+
 
 // Tests
 describe('Entry point', () => {
-	beforeAll(async () => {
-		await import('.');
-	});
+	it('Does not crash', async () => {
+		const promise = (await import('.')).default;
 
-	it('Does not crash', () => {
-		expect(true).toBeTruthy();
+		await expect(promise).resolves.not.toThrow();
 	});
 });
